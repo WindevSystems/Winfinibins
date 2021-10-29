090a9a32570bc0b679acfd0cf87814ab4357726d
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2021-10-29 01:18:50 -0700

    add comments, new default bin, and ascii

--- winfinibins-install.sh
+++ winfinibins-install.sh
@@ -1,14 +1,35 @@
+# winfinibins install script
+# kinda crappy, originally made a while ago
+# ascii text, which i just added in (along with these comments)
+echo "               /$$            /$$$$$$  /$$           /$$ /$$       /$$                    ";
+echo "              |__/           /$$__  $$|__/          |__/| $$      |__/                    ";
+echo " /$$  /$$  /$$ /$$ /$$$$$$$ | $$  \__/ /$$ /$$$$$$$  /$$| $$$$$$$  /$$ /$$$$$$$   /$$$$$$$";
+echo "| $$ | $$ | $$| $$| $$__  $$| $$$$    | $$| $$__  $$| $$| $$__  $$| $$| $$__  $$ /$$_____/";
+echo "| $$ | $$ | $$| $$| $$  \ $$| $$_/    | $$| $$  \ $$| $$| $$  \ $$| $$| $$  \ $$|  $$$$$$ ";
+echo "| $$ | $$ | $$| $$| $$  | $$| $$      | $$| $$  | $$| $$| $$  | $$| $$| $$  | $$ \____  $$";
+echo "|  $$$$$/$$$$/| $$| $$  | $$| $$      | $$| $$  | $$| $$| $$$$$$$/| $$| $$  | $$ /$$$$$$$/";
+echo " \_____/\___/ |__/|__/  |__/|__/      |__/|__/  |__/|__/|_______/ |__/|__/  |__/|_______/ ";
+echo "                                                                                          ";
+echo "                                                                                          ";
 echo "Welcome to the Winfinibins Installer"
 echo "Making Bin Directory"
+# creates the bin directory, where the binaries/commands come from
 mkdir ~/winfinibins
 echo "Done, located in $HOME/winfinibins"
 echo "Adding Lines to .bashrc"
+# Adds some .bashrc lines which makes the gears turn
 echo "export WINFINIBINS_DIR=\"$HOME/winfinibins\"" | tee -a $HOME/.bashrc
+echo "export SNIBINIFNIW_RID=\"$HOME/winfinibins (sorry this isnt reversed, although there is a default reverse bin, you have not yet installed Winfinibins so yeah.)\"" | tee -a $HOME/.bashrc
 echo "export PATH=\"$HOME/winfinibins:$PATH\"" | tee -a $HOME/.bashrc
 echo "...done"
 echo "Installing Default Bins"
+# installs the default bins
 wget -P winfinibins https://raw.githubusercontent.com/WindevStudios/Winfinibins/main/defaultbins/winfinibins-info
+wget -P winfinibins https://raw.githubusercontent.com/WindevStudios/Winfinibins/main/defaultbins/revstring
+# perms change, because bash
 chmod 555 winfinibins/winfinibins-info
-echo "...done, 1 bin installed"
+chmod 555 winfinibins/revstring
+echo "Two default bins installed: info and reverse, by The Winfinibins Authors"
 echo "Winfinibins installed!"
-echo "Please restart your terminal."
+# restart terminal prompt and explanation when/why you have to
+echo "Please restart your terminal. You will only need to do this once when you finish installing Winfinibins for the .bashrc changes this installer has made to take place."
